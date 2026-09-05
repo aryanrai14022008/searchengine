a4d40e9e35ec5d5d98344362cc7a254696660d05
aryan rai <aryanrai1402@gmail.com>
2026-09-05 10:41:38 +0530

    Completely remove slanted white card behind bar wrapper and apply handmade golden wheat craft button style

--- app/page.js
+++ app/page.js
@@ -209,7 +209,7 @@ export default function HomePage() {
                 )}
 
                 <div className="bar-wrapper-visual">
-                  <img src="/sticker_bar.png" alt="HumblBar Clean Protein Bar" className="headline-bar-img" />
+                  <img src="/sticker_bar_clean.png?v=3" alt="HumblBar Clean Protein Bar" className="headline-bar-img" />
                 </div>
 
                 {/* Pure text tagline revealed below the bar - NO BOX, no overlap */}
@@ -336,7 +336,7 @@ export default function HomePage() {
             <div className="terracotta-card">
               {/* Elegant HumblBar Product Background Watermark */}
               <div className="card-bg-bar-watermark" aria-hidden="true">
-                <img src="/sticker_bar.png" alt="" className="watermark-bar-img" />
+                <img src="/sticker_bar_clean.png?v=3" alt="" className="watermark-bar-img" />
               </div>
               
               {/* Card Header & Liquid Progress */}
